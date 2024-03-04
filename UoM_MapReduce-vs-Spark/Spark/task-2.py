a0f8ff60e658a4b4d9dfa5d8edd02308665792c4
vinod.mby@gmail.com <vinod madubashana>
2024-03-04 07:05:22 +0530

    add spark codes

---
+++ UoM_MapReduce-vs-Spark/Spark/task-2.py
@@ -0,0 +1,28 @@
+import argparse
+
+from pyspark.sql import SparkSession
+
+def calculate_year_wise_nsa_delay(data_source, output_uri):
+    with SparkSession.builder.appName("Calculate Year Wise NSA Delay").getOrCreate() as spark:
+        if data_source is not None:
+            flights_df = spark.read.option("header", "true").csv(data_source)
+        flights_df.createOrReplaceTempView("delayed_flights")
+
+        # Create a DataFrame of the top 10 restaurants with the most Red violations
+        year_wise_nsa_delay = spark.sql("""SELECT Year,
+            AVG((CASE WHEN ArrDelay = 0 THEN 0 ELSE NASDelay / ArrDelay END) * 100) AS AvgNSADelayPercentage
+            FROM delayed_flights
+            WHERE Year >= 2003 AND Year <= 2010 GROUP BY Year""")
+
+        # Write the results to the specified output URI
+        year_wise_nsa_delay.write.option("header", "true").mode("overwrite").csv(output_uri)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--data_source', help="The URI for you CSV restaurant data, like an S3 bucket location.")
+    parser.add_argument(
+        '--output_uri', help="The URI where output is saved, like an S3 bucket location.")
+    args = parser.parse_args()
+
+    calculate_year_wise_nsa_delay(args.data_source, args.output_uri)
